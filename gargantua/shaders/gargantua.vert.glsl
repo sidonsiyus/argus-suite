9fed1dc54f23d79a3d611886941a609431dc3385
sidonsiyus <sidonsiyus@gmail.com>
2026-07-23 21:11:03 +0530

    Delete gargantua directory

--- gargantua/shaders/gargantua.vert.glsl
+++
@@ -1,8 +0,0 @@
-// Fullscreen pass. The geometry is a single clip-space triangle pair; all of
-// the work happens in the fragment shader.
-varying vec2 vUv;
-
-void main() {
-    vUv = uv;
-    gl_Position = vec4(position.xy, 0.0, 1.0);
-}
